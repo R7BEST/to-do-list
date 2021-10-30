dddde4d28925c3fa5a21d7faaa21da797f52c7fd
R7BEST <73287074+R7BEST@users.noreply.github.com>
2021-10-30 18:28:56 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+import * as firebase from "firebase";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyChlEoMroS77qAmY6mjOPNDLVsUc8pBjXY",
+  authDomain: "spectagram-cacf8.firebaseapp.com",
+  projectId: "spectagram-cacf8",
+  storageBucket: "spectagram-cacf8.appspot.com",
+  messagingSenderId: "750966426106",
+  appId: "1:750966426106:web:d7bda575fa3bc822173c43"
+};
